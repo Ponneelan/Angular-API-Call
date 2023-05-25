a1c179dafbf1af7c7103ad33d26a6877d6002d24
Ponneelan <ponneelan1305@gmail.com>
2023-05-25 22:47:29 +0530

    endpoint changed

--- src/app/api-call.service.ts
+++ src/app/api-call.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiCallService {
-  private apiUrl = 'https://jsonplaceholder.typicode.com/users'; // Replace with your actual API endpoint
+  private apiUrl = 'http://localhost:3000/getall'; // Replace with your actual API endpoint
   constructor(private http: HttpClient) { }
   getData(){
-    return this.http.get(this.apiUrl);
+     return this.http.get(this.apiUrl);
   }
 }
